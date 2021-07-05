fe874eab746b6173d2ab874693d8bf54a1d37047
Jb Galvo <jb.galvo@foxholeqa.com>
2021-07-05 10:43:01 +0800

    added jb page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts ax-->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
+        <!-- Styles testing lang-->
         <style>
             html, body {
                 background-color: #fff;
