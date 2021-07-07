2a98e3c6c9de649cc4cffdae57a8d3e4cfdbe6b5
vpillorajr <52728161+vpillorajr@users.noreply.github.com>
2021-07-07 15:49:21 +0800

    Finished first draft for Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,10 @@
         <div class="full-height">
             <div class="nav">
                 <div class="top-left links">
-                    <a href="#header"><img src="{{ asset('assets/pics/VP.png') }}"></a> 
+                    <a href="{{ url('/') }}"><img src="{{ asset('assets/pics/VP.png') }}"></a> 
                 </div> 
                 <div class="top-right links">
-                    <a href="#header">Home</a>
+                    <a href="{{ url('/') }}">Home</a>
                     <a href="#about">About</a>
                     <a href="#skill">Skills</a>
                     <a href="#education">Education</a>
@@ -73,19 +73,81 @@
         </div>
 
         <div class="content" id="skill">
-            <h1>SKILLS</h1>
+            <h1 class="flex-center">SKILLS</h1>
+            <p class="flex-center">I am currently living in Naga City. I am studying Bachelor of Science in Computer Science and will graduate in the year 2022. I am practicing and improving my skills.</p>
+            <br />
+            <div class="polaroid" id="first">
+                <p class="logo"><img src="{{ asset('assets/pics/HTML.png')}}"></p>
+                <div class="container">
+                    <p>HTML</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <p class="logo"><img src="{{ asset('assets/pics/CSS.png')}}"></p>
+                <div class="container">
+                    <p>CSS</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <p class="logo"><img src="{{ asset('assets/pics/JS.png')}}"></p>
+                <div class="container">
+                    <p>JavaScript</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <p class="logo"><img src="{{ asset('assets/pics/PHP.png')}}"></p>
+                <div class="container">
+                    <p>PHP</p>   
+                </div>
+            </div>
+            <div class="polaroid" id="fifth">
+                <p class="logo"><img src="{{ asset('assets/pics/XAMPP.png')}}"></p>
+                <div class="container">
+                    <p>Xampp</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <br /><p class="logo"><img src="{{ asset('assets/pics/GHUB.png')}}"></p>
+                <div class="container">
+                    <p>GitHub</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <p class="logo"><img src="{{ asset('assets/pics/GLAB.png')}}"></p>
+                <div class="container">
+                    <p style="padding-bottom: 8px">GitLab</p>   
+                </div>
+            </div>
+            <div class="polaroid" id="eight">
+                <p class="logo"><img src="{{ asset('assets/pics/CPLUS.png')}}"></p>
+                <div class="container">
+                    <p>C++</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <p class="logo"><img src="{{ asset('assets/pics/BSTRAP.png')}}"></p>
+                <div class="container">
+                    <p>Bootstrap</p>   
+                </div>
+            </div>
+            <div class="polaroid">
+                <p class="logo"><img src="{{ asset('assets/pics/JAVA.png')}}"></p>
+                <div class="container">
+                    <p>Java</p>   
+                </div>
+            </div>
         </div>
 
         <div class="content" id="education">
             <h1 class="flex-center">EDUCATION</h1>
-            <p class="flex-center">Through the years in my life I experience knowledge that really strengthen my<br /> skills and my confidence to reach my goals</p>
+            <p class="flex-center">Through the years in my life I experience knowledge that really strengthen my<br /> skills and my confidence to reach my goals.</p>
             <!-- Schools -->
             <div class="polaroid">
                 <p class="logo"><img src="{{ asset('assets/pics/TMES.png')}}"></p>
                 <div class="container">
                     <p><b>School Name:</b> Teodora Moscoso Elementary School</p>
                     <p><b>Year Started:</b> 2011</p>
-                    <p><b>Year Graduated:</b> 2012</p>
+                    <p class="first"><b>Year Graduated:</b> 2012</p>
                 </div>
             </div>
 
@@ -118,11 +180,54 @@
         </div>
 
         <div class="content" id="experience">
-            <h1>EXPERIENCE</h1>
+            <h1 class="flex-center">EXPERIENCE</h1>
+            <div class="text-container ">
+                <div class="time"><b>2019 - PRESENT</b></div>
+                <h3><b>Freelance Web Developer</b></h3>
+                <p>Working happily on my own web projects</p>
+                <div class="time"><b>2018 - 2019</b></div>
+                <h3><b>Lead Web Developer</b></h3>
+                <p>Beautiful project for a major digital agency</p>
+            </div> <!-- end of text-container -->
+            
+            <div class="text-container">
+                <div class="time"><b>2017 - 2018</b></div>
+                <h3><b>Senior Web Designer</b></h3>
+                <p>Inhouse web designer for ecommerce firm</p>
+                <div class="time"><b>2016 - 2017</b></div>
+                <h3><b>Junior Web Designer</b></h3>
+                <p>Junior web designer for small web agency</p>
+            </div> <!-- end of text-container -->
+            
         </div>
+        
 
         <div class="content" id="portfolio">
-            <h1>PORTFOLIO</h1>
+            <h1 class="flex-center">PORTFOLIO</h1>
+            <p class="flex-center">Listed below are some of the most representative projects I've worked on.</p>
+            <br />
+
+            <div class="text-container">
+                <div class="image-container">
+                    <p><img src="{{ asset('assets/pics/portfolio_1.png')}}"></p>
+                </div>
+                <p align="center"><b>Project:</b> A clinic front page with bootstrap.</p>
+            </div>
+
+            <div class="text-container">
+                <div class="image-container">
+                    <p><img src="{{ asset('assets/pics/portfolio_2.png')}}"></p>
+                </div>
+                <p align="center"><b>Project:</b> An eCommerce webpage of a company seling gadgets such as phones.</p>
+            </div>
+
+            <div class="text-container">
+                <div class="image-container">
+                    <p><img src="{{ asset('assets/pics/portfolio_3.png')}}"></p>
+                </div>
+                <p align="center"><b>Project:</b> A simple tourism page about Bicol.</p>
+            </div>
+
         </div>
 
         <div class="content" id="contact">
